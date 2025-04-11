6cd4d01a3516b12759220625b772857fe13da93f
SaiManikantha <ssanagavarpu@deloitte.com>
2025-04-11 14:49:53 +0530

    .

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,5 @@
 export default async function decorate(block) {
-    iconDiv.innerHTML = `<img src="../../icons/search.svg" width="15" height="31.75" alt="Arrow Icon">`;
+    iconDiv.innerHTML = `<img src="../../icons/search.svg" width="15" height="31.75" alt="Arrow Icon">
+    
+    <h1>hellolillytested</h1>`;
 }
