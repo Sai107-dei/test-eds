1dcbdf91c7fadfe2dad819ddcafd5014c8cd3ea7
SaiManikantha <ssanagavarpu@deloitte.com>
2025-04-21 18:21:41 +0530

    .

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -97,6 +97,7 @@ function attachEventListners(main) {
     'aue:content-remove',
     'aue:content-copy',
   ].forEach((eventType) => main?.addEventListener(eventType, async (event) => {
+    alert('something');
     event.stopPropagation();
     const applied = await applyChanges(event);
     if (!applied) window.location.reload();
