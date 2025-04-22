43d6ea97ef14c72d396934e89b51b5b3a711ff06
SaiManikantha <ssanagavarpu@deloitte.com>
2025-04-22 14:47:47 +0530

    .

--- blocks/gated-sections/gated-sections.js
+++ blocks/gated-sections/gated-sections.js
@@ -1,3 +1,10 @@
 export default function decorate(block) {
-  block.innerHTML = `<h1>hie</h1>`;
+  block.innerHTML = `<h2>Sign Up</h2>
+    <form >
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Submit">
+    </form>`;
 }
