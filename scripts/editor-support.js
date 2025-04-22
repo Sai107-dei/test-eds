90ca7e324c35e2acde5709fd8199735adaedb495
SaiManikantha <ssanagavarpu@deloitte.com>
2025-04-22 16:30:06 +0530

    .

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -13,7 +13,7 @@ import { decorateMain } from './scripts.js';
 function addMetaTag() {
   const meta = document.createElement('meta');
   meta.name = "urn:adobe:aue:config:disable";
-  meta.content = "publish-preview";
+  meta.content = "publish-live";
   document.head.appendChild(meta);
  
 }
