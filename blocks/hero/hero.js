a182eeedc6e22622017a5adb4cae4320db8f04c8
SaiManikantha <ssanagavarpu@deloitte.com>
2025-04-26 12:57:36 +0530

    .

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,8 +1,5 @@
 import { decorateIcons } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-    const brandWrapperDesktop = document.createElement('div');
-    brandWrapperDesktop.innerHTML = ` <a class="brand-logo-wrapper" href="/"><img src="../../icons/search.svg" alt="Pricefx" loading="lazy"></a>`;
-    decorateIcons(brandWrapperDesktop, '', 'Pricefx');
-
+   
 }
