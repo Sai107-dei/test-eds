fac4f86dd120b60a6364e6a1731620da0568050f
SaiManikantha <ssanagavarpu@deloitte.com>
2025-04-28 23:27:45 +0530

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,12 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
-  },
+    
+        'xwalk/max-cells': ['error', {
+          '*': 3,
+          'card': 4,
+          'cards': 2,
+        }]
+      }
+   
 };
